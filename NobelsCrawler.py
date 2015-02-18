9598ee6278fd95c7f8df5f434fbcb31f05c744db
luca-piccinelli <luca.piccinelli@studiofarma.it>
2015-02-18 15:40:46 +0100

    funzionante su cgm... ne trova solo 6 su 8!!!

--- NobelsCrawler.py
+++ NobelsCrawler.py
@@ -6,6 +6,7 @@ Created on Sun Feb 15 17:20:24 2015
 """
 
 import urllib2
+import urlparse
 import re
 from HTMLParser import HTMLParser
 
@@ -19,7 +20,7 @@ def extract_list_from_html(html, regex):
         match = regex.findall(line)
         if match:
             for group in match:
-                l.append(htmlParser.unescape(group))
+                l.append(str(htmlParser.unescape(group)))
                 
     return l
 
@@ -28,35 +29,85 @@ def get_nobel_prizes_list():
     nobel_prizes_url = 'http://www.nobelprize.org/nobel_prizes/medicine/laureates/'
     response = urllib2.urlopen(nobel_prizes_url)
     nobel_html = response.read()
-    return extract_list_from_html(nobel_html, r'<a\s+href="[^\"]*facts.html">([^<>]+)<\/a>')
+    nobels_list = extract_list_from_html(nobel_html, r'<a\s+href="[^\"]*facts.html">([^<>]+)<\/a>')
+    nobel_names_list1 = []
+    for x in nobels_list:
+        name = x.split(' ')
+        if len(name[0]) > 2:
+            nobel_names_list1.append(name[0])
+        else:
+            nobel_names_list1.append(name[1])
+    
+    nobel_names_list2 = []
+    for x in nobels_list:
+        name = x.split(' ')
+        if len(name[-1]) > 2 and name[-1][-1] != '.':
+            nobel_names_list2.append(name[-1])
+        else:
+            nobel_names_list2.append(name[-2])
+            
+    nobel_names_list3 = []
+    for x in nobels_list:
+        name = x.split(' ')
+        name_str = ''
+        for n in name:
+            if len(n) > 2 and n[-1] != '.':
+                name_str += n + ' '
+                
+        nobel_names_list3.append(name_str[:-1])
+        
+    nobel_names_list4 = [' '.join([x, y]) for x, y in zip(nobel_names_list1, nobel_names_list2) ]
+        
+    
+    with open(r'd:\nobel_list.txt', 'w') as f:
+        f.write('\n'.join(nobels_list))
+        f.write('\n\n\n')
+        f.write('\n'.join(nobel_names_list3))
+        f.write('\n\n\n')
+        f.write('\n'.join(nobel_names_list4))
+    
+    return nobels_list + nobel_names_list4 + nobel_names_list3
     
     
 def find_page_links(html):
-    return extract_list_from_html(html, r'<a\s+href="((?!http)/[^\"]{2,})">')
+    return extract_list_from_html(html, r'<a\s+[^>]*href="((?!http)[^\"#]{2,})"[^>]*>')
     
 def crawl(domain, url, crawl_dict, fn):
-    if url not in crawl_dict:
+    if url not in crawl_dict and not url.endswith('.jpg') and not url.endswith('.pdf') and not url.endswith('.png'):
         crawl_dict[url] = 1
         print 'crawling {0}'.format(url)
-        response = urllib2.urlopen(url)
-        html = response.read()
-        fn(html)
-        for link in find_page_links(html):
-            crawl(domain, '{0}{1}'.format(domain, link), crawl_dict, fn)
+        try:
+            response = urllib2.urlopen(url)
+            if(response.info().maintype == 'text'):
+                html = response.read()
+                fn(url, html)
+                for link in find_page_links(html):
+                    crawl(domain, urlparse.urljoin(url, link), crawl_dict, fn)
+        except urllib2.URLError:
+            print 'not an url: {0}'.format(url)
             
-def contains_nobel_name(html):
+def contains_nobel_name(page_url, html):
     global nobel_prizes_list
+    global filename
     
     for prize in nobel_prizes_list:
         if prize in html:
-            print 'this page contains: {0}'.format(prize)
+            l = 'the page {0} page contains: {1}'.format(page_url, prize)
+            print l
+            with open(filename, 'a') as f:
+                f.write(l + '\n')
+            
 
 if __name__ == '__main__':
+    global filename
     global nobel_prizes_list
+    
+    filename = r'd:\nobel.txt'
+    
     nobel_prizes_list = get_nobel_prizes_list()
-    print len(nobel_prizes_list)    
+    print len(nobel_prizes_list)
        
-    crawl('http://www.nobelprize.org', 'http://www.nobelprize.org/nobel_prizes/medicine/laureates/', {}, contains_nobel_name)
+    crawl('http://cgmone.cgm.ag/cgm_com/', 'http://cgmone.cgm.ag/cgm_com/index.en.jsp', {}, contains_nobel_name)
     
     
     
